cde6e31dd6e41b8873a0067bad485d3ea3322872
ValeriaRodriguezJ <rodriguz95@gmail.com>
2024-08-26 16:00:19 -0500

    feat: add logic about select random monsters after select first monster

--- src/app/components/battle/computer-card/ComputerCard.tsx
+++ src/app/components/battle/computer-card/ComputerCard.tsx
@@ -1,9 +1,48 @@
 "use client";
 
+import { useSelector } from "react-redux";
 import { MonsterBattleCard } from "../ui/monster-battle-card/MonsterBattleCard";
+import {
+  selectMonsters,
+  selectSelectedComputerMonster,
+  selectSelectedMonster,
+} from "@/reducers/monsters/monsters.selectors";
+import { useCallback, useEffect } from "react";
+import { useAppDispatch } from "@/store/hooks";
+import { setSelectedComputerMonster } from "@/reducers/monsters/monsters.actions";
 
 const ComputerCard = () => {
-  return <MonsterBattleCard title="Computer" />;
+  const selectedMonster = useSelector(selectSelectedMonster);
+  const listMonsters = useSelector(selectMonsters);
+  const computerCurrentMonster = useSelector(selectSelectedComputerMonster);
+  const dispatch = useAppDispatch();
+
+  const calculateRandomMonster = useCallback(() => {
+    const newListMonstersWithoutP1 = listMonsters.filter(
+      (item) => item.id !== selectedMonster?.id
+    );
+
+    const indexRandom = Math.floor(
+      Math.random() * newListMonstersWithoutP1.length
+    );
+
+    const computerMonster = newListMonstersWithoutP1[indexRandom];
+
+    dispatch(setSelectedComputerMonster(computerMonster));
+  }, [dispatch, listMonsters, selectedMonster]);
+
+  useEffect(() => {
+    if (selectedMonster) {
+      calculateRandomMonster();
+    }
+  }, [selectedMonster, calculateRandomMonster]);
+
+  return (
+    <MonsterBattleCard
+      title={computerCurrentMonster?.name || "Computer"}
+      monster={computerCurrentMonster}
+    />
+  );
 };
 
 export { ComputerCard };
