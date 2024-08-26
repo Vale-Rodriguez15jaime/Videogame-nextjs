1d3cf1fa47026027d61daf3141753bd0713253fe
ValeriaRodriguezJ <rodriguz95@gmail.com>
2024-08-26 16:01:51 -0500

    feat: call winner from redux in the component winner and add test

--- src/app/components/winner/Winner.tsx
+++ src/app/components/winner/Winner.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useSelector } from "react-redux";
 import { WinnerDisplay } from "./ui/winner-display/WinnerDisplay";
+import { selectWinner } from "@/reducers/monsters/monsters.selectors";
 
 const Winner = () => {
-  const battleWins = null;
-  return <>{battleWins && <WinnerDisplay text={battleWins} />}</>;
+  const battleWins = useSelector(selectWinner);
+  return <>{battleWins && <WinnerDisplay text={battleWins.name} />}</>;
 };
 
 export { Winner };
--- src/app/components/winner/ui/winner-display/WinnerDisplay.spec.tsx
+++ src/app/components/winner/ui/winner-display/WinnerDisplay.spec.tsx
@@ -1,10 +1,15 @@
 import { WinnerDisplay } from "@/app/components/winner/ui/winner-display/WinnerDisplay";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import monstersData from "../../../../../../data/monsters.json";
 
 describe("WinnerDisplay", () => {
   it("should render the winner text correctly", () => {
-      // @TODO
+    const monster = monstersData.monsters[0];
+    render(<WinnerDisplay text={monster.name} />);
+    const winnerText = screen.getByText(`${monster.name} wins!`);
+
+    expect(winnerText).toBeInTheDocument();
   });
 
   it("should render the default text when no winner text is provided", () => {
